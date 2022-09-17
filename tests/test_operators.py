adc38405c2603bacbfd3cb4cff5e4b38912efef7
Sakigami Yang <sakigami@163.com>
2022-09-17 22:10:04 +0800

    finish task 0.2 (#2)

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -104,48 +104,52 @@ def test_sigmoid(a):
     * It crosses 0 at 0.5
     * it is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
+    assert sigmoid(-0.1) < 0.5
+    assert sigmoid(0.1) > 0.5
+    assert sigmoid(a) <= sigmoid(a + 0.1)
+    assert sigmoid(a) >= sigmoid(a - 0.1)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a, b, c):
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert not (a < b < c) or a < c
 
 
 @pytest.mark.task0_2
-def test_symmetric():
+@given(small_floats, small_floats)
+def test_symmetric(a, b):
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert_close(mul(a, b), mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute():
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a, b, c):
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert_close(a * (b + c), a * b + a * c)
 
 
 @pytest.mark.task0_2
-def test_other():
+@given(small_floats)
+def test_other(a):
     """
     Write a test that ensures some other property holds for your functions.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    if a > 0:
+        assert relu(a) == a
+    else:
+        assert relu(a) == 0.0
 
 
 # ## Task 0.3  - Higher-order functions
